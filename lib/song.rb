b634c9eeb1be640a6efe58f4359a8187bfebb1ee
Norberto Santiago <norbertosantiago82@gmail.com>
2020-02-08 04:29:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,6 +28,6 @@ class Song
   
   def self.genre_count(genre_count)
     genre_count = {}
-    @@genres.each {|genre| self[genre]=genre_count}
+    @@genres.each {|genre| self[genre]= }
   end
 end 
